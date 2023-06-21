2ba708b0d663eddfd89044a2dbea1ada74a8bd07
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-22 02:22:49 +0300

    Fetch data from api

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = 'https://randomuser.me/api/?results=5'
 
 const initialState = {
     users: [],
@@ -6,15 +9,46 @@ const initialState = {
     error: null
 }
 
+export const getUsers = createAsyncThunk('users/getUsers', async()=>{
+    try{
+        const jsonData = await axios.get(url)
+        return jsonData.data
+
+    } catch(err){
+        return err.message}
+
+})
+
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
-
+        array: ()=>{
+            "kjj"
+        }
     },
-    extraReducers:{
+    extraReducers(builder){
+        builder
+
+        .addCase(getUsers.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(getUsers.fulfilled, (state, action)=>{
+            console.log(action)
+            state.users = action.payload.results
+            state.isLoading = false
+            console.log(state.users)
+        })
+        .addCase(getUsers.rejected, (state, action)=>{
+            state.isLoading = false
+            state.error = action.error.message
+        })
+
+       
         
+
     }
 })
+export const {array} = usersSlice.actions
 
 export default usersSlice.reducer
